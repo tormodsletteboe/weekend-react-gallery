50a04bc8fdaa07546bedec4e316fe8fa109ebca0
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-21 18:17:17 -0500

    put it on aws

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -4,11 +4,11 @@ const galleryItems = [
     { id: 3, path: 'https://picsum.photos/id/1001/200', description: 'Dad carrying son on beach', likes: 5 },
     { id: 4, path: 'https://picsum.photos/id/237/200', description: 'Black labrador puppy looking at camera', likes: 5 },
     { id: 5, path: 'https://picsum.photos/id/1000/200', description: 'Christian cross on windy snowy mountain top', likes: 5 },
-    { id: 6, path: 'https://picsum.photos/id/236/200', description: 'House on the rocks', likes: 5 },
-    { id: 7, path: 'https://picsum.photos/id/10/200', description: 'Forest overlooking a fjord', likes: 5 },
-    { id: 8, path: 'https://picsum.photos/id/1001/200', description: 'Dad carrying son on beach', likes: 5 },
-    { id: 9, path: 'https://picsum.photos/id/237/200', description: 'Black labrador puppy looking at camera', likes: 5 },
-    { id: 10, path: 'https://picsum.photos/id/1000/200', description: 'Christian cross on windy snowy mountain top', likes: 5 }
+    { id: 6, path: 'https://picsum.photos/id/235/200', description: 'House on the rocks', likes: 5 },
+    { id: 7, path: 'https://picsum.photos/id/15/200', description: 'Forest overlooking a fjord', likes: 5 },
+    { id: 8, path: 'https://picsum.photos/id/101/200', description: 'Dad carrying son on beach', likes: 5 },
+    { id: 9, path: 'https://picsum.photos/id/26/200', description: 'Black labrador puppy looking at camera', likes: 5 },
+    { id: 10, path: 'https://picsum.photos/id/1/200', description: 'Christian cross on windy snowy mountain top', likes: 5 }
 
 
    
