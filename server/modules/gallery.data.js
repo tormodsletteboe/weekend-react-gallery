be93387caa400e582ef9cf826a5c82c82f231ed2
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-21 12:14:14 -0500

    get router works, include render to dom part

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'https://picsum.photos/100', description: 'who knows', likes: 5 }
+    { id: 1, path: 'https://picsum.photos/id/236/200', description: 'House on the rocks', likes: 5 },
+    { id: 2, path: 'https://picsum.photos/id/10/200', description: 'Forest overlooking a fjord', likes: 5 },
+    { id: 3, path: 'https://picsum.photos/id/1001/200', description: 'Dad carrying son on beach', likes: 5 },
+    { id: 4, path: 'https://picsum.photos/id/237/200', description: 'Black labrador puppy looking at camera', likes: 5 },
+    { id: 5, path: 'https://picsum.photos/id/1000/200', description: 'Christian cross on windy snowy mountain top', likes: 5 }
+
+
+   
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import './App.css';
 import {useState,useEffect} from 'react';
+import axios from "axios";
+import Button from 'react-bootstrap/Button';
 
 function App() {
 
   let [listOfGalleryItems,setlistOfGalleryItems] = useState([]);
 
-  useEffect((evt)=>{
-    //getGalleryItems();
+  //on load
+  useEffect(()=>{
+    console.log('in useeffect');
+    getGalleryItems();
   },[]);
+
+  //get gallery items from gallery.data.js
+  let getGalleryItems = ()=>{
+    console.log('in getGalleryItems');
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    })
+    .then((response)=>{
+      console.log('data',response.data)
+      setlistOfGalleryItems(response.data);
+      
+    })
+    .catch((err)=>{
+      console.error('axios GET error',err);
+    });
+  };
 
     return (
       <div className="App">
@@ -16,15 +37,27 @@ function App() {
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        
-        <img src="https://picsum.photos/id/236/200"/>
-        <img src="https://picsum.photos/id/10/200"/>
-        <img src="https://picsum.photos/id/1001/200"/>
-        <img src="https://picsum.photos/id/237/200"/>
-        <img src="https://picsum.photos/id/1000/200"/>
-        
+        {/* <img src = "https://picsum.photos/id/236/200"/> */}
+        {listOfGalleryItems.map(gallItem=>(
+          <div className={'ImageDiv'} key={gallItem.id} >
+            <figure>
+              <img src={gallItem.path} alt={gallItem.description} />
+              <figcaption>
+                {gallItem.description}
+              </figcaption>
+              <Button variant="dark" size="sm">Primary</Button>
+            </figure>
+            
+          </div>
+        ))}
       </div>
     );
 }
 
 export default App;
+
+
+// <figure key={item.id}>
+//             <img src={item.path} alt={item.description}/>
+//             <figcaption>Likes: {item.likes}</figcaption>
+//           </figure>
