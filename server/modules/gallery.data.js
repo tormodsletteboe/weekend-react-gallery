441ee760064cde5b259c86e7ef8c848541f2543c
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-21 11:06:38 -0500

    stopping to branch

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,6 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'https://picsum.photos/100', description: 'who knows', likes: 5 }
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
+import {useState,useEffect} from 'react';
 
 function App() {
+
+  let [listOfGalleryItems,setlistOfGalleryItems] = useState([]);
+
+  useEffect((evt)=>{
+    //getGalleryItems();
+  },[]);
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        
+        <img src="https://picsum.photos/id/236/200"/>
+        <img src="https://picsum.photos/id/10/200"/>
+        <img src="https://picsum.photos/id/1001/200"/>
+        <img src="https://picsum.photos/id/237/200"/>
+        <img src="https://picsum.photos/id/1000/200"/>
+        
       </div>
     );
 }
